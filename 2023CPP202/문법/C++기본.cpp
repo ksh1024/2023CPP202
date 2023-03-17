76e967d021a122d03ab5801af1403ac61807c399
ksh25 <ksh251024@gmail.com>
2023-03-17 09:50:28 +0900

    변수 출력하기

--- 2023CPP202/문법/C++기본.cpp
+++ 2023CPP202/문법/C++기본.cpp
@@ -5,9 +5,12 @@ using namespace std;
 //이름공간 (변수 이름 중복 방지)
 
 int main() {
+
+	int year = 2023;
+
 	// << : 출력연산자
 	// endl :  개행 (줄바꿈)
-	std::cout << "hello world" << std::endl;
+	cout << "hello world " << year << endl;
 	
 	return 0;
 }
