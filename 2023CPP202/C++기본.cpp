950cde1599e0fa8a3f47f735fa6e3074c465ca37
ksh25 <ksh251024@gmail.com>
2023-03-17 09:45:44 +0900

    개행객체endl & namespace

--- 2023CPP202/C++기본.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include <stdio.h>
-
-int main() {
-	std::cout<<"hello world\n";
-	
-	return 0;
-}
--- 2023CPP202/문법/C++기본.cpp
+++ 2023CPP202/문법/C++기본.cpp
@@ -1,9 +1,13 @@
 ﻿#include <iostream>
 //cout객체사용
 
+using namespace std;
+//이름공간 (변수 이름 중복 방지)
+
 int main() {
 	// << : 출력연산자
-	std::cout << "hello world\n";
+	// endl :  개행 (줄바꿈)
+	std::cout << "hello world" << std::endl;
 	
 	return 0;
 }
