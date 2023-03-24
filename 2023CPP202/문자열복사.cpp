ede1cd2819e4353ccf8db6c0c431834dfac06995
ksh25 <ksh251024@gmail.com>
2023-03-24 09:05:18 +0900

    C 스타일 문자열 복사

---
+++ 2023CPP202/문자열복사.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main() {
+	char str1[30] = "";
+	char str2[30] = "Jiwoo";
+
+	for (int i = 0; str2[i] != '\0'; i++) { ///null이 아닐때까지
+		str1[i] = str2[i];
+	}
+	printf("%s \n", str1);
+
+	return 0;
+}
