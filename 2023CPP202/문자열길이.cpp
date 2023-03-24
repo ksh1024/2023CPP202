3321f35b2a3a7fc636ae562e7c1f11de8f656427
ksh25 <ksh251024@gmail.com>
2023-03-24 09:45:56 +0900

    C스타일 문자열길이strlen(()

--- 2023CPP202/문자열길이.cpp
+++ 2023CPP202/문자열길이.cpp
@@ -9,12 +9,7 @@
 using namespace std;
 int main() {
 	char str1[30] = "jiwoo";
-	int len = 0;
-
-	for (int i = 0; str1[i] != '\0'; i++) {
-		len++;
-	}
-
+	int len = strlen(str1);
 	printf("문자열의 길이는 %d \n", len);
 
 	return 0;
