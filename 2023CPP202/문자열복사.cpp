5d0a53cb2a1f4c726c237a27f7e3fdf5771fb365
ksh25 <ksh251024@gmail.com>
2023-03-24 09:15:30 +0900

    C++스타일 문자열 복사

--- 2023CPP202/문자열복사.cpp
+++ 2023CPP202/문자열복사.cpp
@@ -1,12 +1,14 @@
-﻿#include <stdio.h> 
-#include <string.h> //문자열 함수가 들어있음
+﻿#include <iostream> 
+#include <string> //str:string 클래스
 
 int main() {
-	char str1[30] = "";
-	char str2[30] = "Jiwoo";
+	std::string str1;
+	std::string str2="jiwoo";
 
-	strcpy(str1,str2); //str1에 str2내용을 넣음
-	printf("%s \n", str1);
+	str1 = str2;
+
+	std::cout << str1 << std::endl;
+
 
 	return 0;
 }
