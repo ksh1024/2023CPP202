4244a3634f2eae8595e4c7a76f326ce9b66d80d4
ksh25 <ksh251024@gmail.com>
2023-03-24 09:44:44 +0900

    C스타일 문자열 길이

---
+++ 2023CPP202/문자열길이.cpp
@@ -0,0 +1,21 @@
+﻿//C
+#include<stdio.h>
+#include<string.h>
+
+//C++
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main() {
+	char str1[30] = "jiwoo";
+	int len = 0;
+
+	for (int i = 0; str1[i] != '\0'; i++) {
+		len++;
+	}
+
+	printf("문자열의 길이는 %d \n", len);
+
+	return 0;
+}
--- 2023CPP202/문자열복사.cpp
+++ 2023CPP202/문자열복사.cpp
@@ -1,14 +1,1 @@
-﻿#include <iostream> 
-#include <string> //str:string 클래스
-
-int main() {
-	std::string str1;
-	std::string str2="jiwoo";
-
-	str1 = str2;
-
-	std::cout << str1 << std::endl;
-
-
-	return 0;
-}
+﻿
