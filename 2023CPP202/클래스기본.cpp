6b01e5f5b5424f6d45bd5cdf7fc09367066e4c96
ksh25 <ksh251024@gmail.com>
2023-04-04 13:54:35 +0900

    private public순서 바꾸기

--- 2023CPP202/클래스기본.cpp
+++ 2023CPP202/클래스기본.cpp
@@ -8,38 +8,36 @@ using namespace std;
 //구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로 합시다
 //class는 디폴트가 private (struct는 디폴트가 public)
 class Student {
+public:
+	//생성자 (constructor) : 객체가 생성될 때 호출되는 함수
+	//생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의된다
+	Student() {
+		number = 2222;
+		name = "아이유";
+		phone = "010-1234-1234";
+		major = "뉴미디어소프트웨어";
+		address = "관악구";
+	}
+	//class 멤버 함수를 가질 수 있다
+	void print() {
+		cout << "학번 : " << number << "\n이름 : " << name << "\n번호 : " << phone << endl;
+		cout << "전공 : " << major << "\n주소 : " << address << endl;
+	}
 	// 성능 때문에 number를 문자열로 하지 않음
 	//일반적으로 문자열은 정수형 데이터보다 많은 메모리 공간을 요구하며
 	//정수는 비교연산을 한 번에 할 수 있으나, 문자열은 글자 수만큼 반복하여 비교해야 한다
-public:
+private:
 	int number;
 	string name;
 	string phone;
 	string major;
 	string address;
-
-	//생성자 (constructor) : 객체가 생성될 때 호출되는 함수
-	//생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의된다
-	// Student(){}
-	// 
- 	//class 멤버 함수를 가질 수 있다
-
-	void print() { 
-		cout << "학번 : " << number << "\n이름 : " << name << "\n번호 : " << phone << endl;
-		cout << "전공 : " << major << "\n주소 : " << address << endl;
-	}
 };
-int main() {
-	//자료형 : Student(Student앞에 class 생략 가능) 
+int main(void)
+{
+	// 자료형 : Student(class 생략 가능)
 	Student a;
-	a.number = 2222;
-	a.name = "아이유";
-	a.phone = "010-1234-1234";
-	a.major = "뉴미디어소프트웨어";
-	a.address = "관악구";
 
 	a.print();
-
-
 	return 0;
 }
