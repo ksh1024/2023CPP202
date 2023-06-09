43ea58363507a82212295d01663bc977c19642f1
ksh25 <ksh251024@gmail.com>
2023-06-09 09:46:04 +0900

    부모의 멤버변수/함수 사용

--- 2023CPP202/상속.cpp
+++ 2023CPP202/상속.cpp
@@ -1,11 +1,14 @@
 ﻿#include <iostream>
+#include <string>
 
 using namespace std;
 
 class Animal {
 public:
-	Animal() {
+	Animal(int age, string name):age_(age),name_(name) {
 		cout << "동물 생성자" << endl;
+		cout << "나이 " << age_ << endl;
+		cout << "이름 " << name_ << endl;
 	}
 	~Animal() {
 		cout << "동물 소멸자" << endl;
@@ -21,16 +24,17 @@ public:
 	}
 private:
 	int age_;
-	int name_;
+	string name_;
 
 };
 
 
 class Crane : public Animal{
 public:
-	//별도의 언급이 없어도 매개변수가 없는 부모생성자가 호출된다
-	Crane() {
+	Crane(int age, string name,int len_length) : Animal(age,name) {
 		cout << "두루미 생성자" << endl;
+		leg_length_ = len_length;
+		cout << "다리 길이 " << leg_length_ << endl;
 	}
 	~Crane() {
 		cout << "두루미 소멸자" << endl;
@@ -41,8 +45,8 @@ private:
 
 
 int main() {
-	Crane* crane = new Crane();
-
+	Crane* crane = new Crane(3, "지우",108);//부모의 멤버함수를 사용
+	crane->Eat();	//부모의 멤버함수를 사용
 	delete crane;
 	return 0;
 }
