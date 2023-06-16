2904eb19b379e4900a6300db26807d3e8d1b6a36
ksh25 <ksh251024@gmail.com>
2023-06-16 09:43:23 +0900

    소멸자에는 반드시 virtual을 넣자

--- 2023CPP202/상속.cpp
+++ 2023CPP202/상속.cpp
@@ -8,19 +8,14 @@ public:
 	Animal(int age, string name):age_(age),name_(name) {
 		cout << "동물 생성자" << endl;
 	}
-	~Animal() {
+	//소멸자에는 무조건 virtual을 넣자(메모리 누수 방지)
+	virtual ~Animal() {
 		cout << "동물 소멸자" << endl;
 	}
-
-	virtual void Bark() { 
-		cout << "동물 짖는다" << endl;
-	}
-	virtual void Eat() {
-		cout << "동물 먹는다" << endl;
-	}
-	virtual void Hunt() {
-		cout << "동물 사냥한다" << endl;
-	}
+	//동적 바인딩 (가상함수)
+	virtual void Bark() { 	cout << "동물 짖는다" << endl;}
+	virtual void Eat() {	cout << "동물 먹는다" << endl;}
+	virtual void Hunt() {	cout << "동물 사냥한다" << endl;}
 private:
 	int age_;
 	string name_;
@@ -34,7 +29,7 @@ public:
 		cout << "두루미 생성자" << endl;
 		leg_length_ = len_length;
 	}
-	~Crane() {
+	virtual ~Crane() {
 		cout << "두루미 소멸자" << endl;
 	}
 	//오타같은 실수를 방지하지 위해 override 키워드를 사용(Java의 @Override)
