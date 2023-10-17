daf050d84562f64f1c96460dfd7670486f670f97
ksh25 <ksh251024@naver.com>
2023-10-17 14:36:09 +0900

    auto를 통하여 자료형을 자동으로 맞춰준다

--- 2023CPP202/범위기반for문.cpp
+++ 2023CPP202/범위기반for문.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main() {
 	double arr[4] = { 1.1, 2.2, 3.3, 4.4 };
-	for (double x : arr) {
+
+	//누가봐도 a의 자료형은 int이다.
+	auto a = 4;
+	
+	//x의 자료형은 double로 자동설정
+	for (auto x : arr) {
 		cout << x << endl;
 	}
 
